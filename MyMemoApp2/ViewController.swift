8584aa4c923db877c0b8a1de1eb05b89bb0b2265
fumiya_N <fn.apple.creator@gmail.com>
2020-05-07 22:42:08 +0900

    Initial Commit

---
+++ MyMemoApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemoApp2
+//
+//  Created by Fumiya.N on 2020/05/07.
+//  Copyright © 2020 Fumiya.N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
